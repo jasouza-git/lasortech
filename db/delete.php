0f9b2c85b4ad9d3c1566f686151bbfe0923da56d
ChenLin Wang <wangchenlin2001@icloud.com>
2025-07-17 23:48:27 +0800

    add: finish the login system and tested

--- db/delete.php
+++ db/delete.php
@@ -1,16 +1,16 @@
 <?php
-require_once __DIR__ . "/basic.php";
+require_once "basic.php";
 require_once "db.php";
 class DB_DELETE extends DB {
 
-    public function delete(array $data, string $table) {
+    public function delete(array $data, string $table, string $custom_id = null) {
         required(
             isset($data['ids']) && count($data['ids']) > 0, 
             15, 
             "provide at least one id of $table to delete"
         );
-        
-        $combined = build_delete_sql($data['ids'], $table);
+
+        $combined = build_delete_sql($data['ids'], $table, $custom_id);
         return $this->execute($combined);
     }
 }
--- db/struct.sql
+++ db/struct.sql
@@ -1,17 +1,11 @@
-START TRANSACTION;
-
-CREATE DATABASE IF NOT EXISTS lasortech
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_general_ci;
-
-USE lasortech;
+SET GLOBAL time_zone = '+00:00';
+SET time_zone = '+00:00';
 
 CREATE TABLE `employees` (
 	`id` varchar(64) NOT NULL,
-	`name` text NOT NULL,
-	`contact_number` text NOT NULL,
-	`email` text NOT NULL,
-	`messenger_id` text,
+	`name` varchar(64) NOT NULL,
+	`contact_number` varchar(20) NOT NULL,
+	`messenger_id` varchar(255),
 	`avatar` text,
 	`description` text,
 	`working` boolean NOT NULL DEFAULT true,
@@ -20,11 +14,21 @@ CREATE TABLE `employees` (
 	CONSTRAINT `employees_id` PRIMARY KEY(`id`)
 );
 
+CREATE TABLE `users` (
+	`id` varchar(64) NOT NULL,
+	`email` varchar(64) NOT NULL,
+	`password_hashed` varchar(255) NOT NULL,
+	`update_at` timestamp(6) NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP(6),
+	`create_at` timestamp(6) NOT NULL DEFAULT (now()),
+	CONSTRAINT `users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `users_email_unique` UNIQUE(`email`)
+);
+
 CREATE TABLE `customers` (
 	`id` varchar(64) NOT NULL,
-	`name` text NOT NULL,
-	`contact_number` text NOT NULL,
-	`email` text NOT NULL,
+	`name` varchar(64) NOT NULL,
+	`contact_number` varchar(20) NOT NULL,
+	`email` varchar(64) NOT NULL,
 	`messenger_id` text,
 	`description` text,
 	`update_at` timestamp(6) NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP(6),
@@ -35,10 +39,10 @@ CREATE TABLE `customers` (
 CREATE TABLE `items` (
 	`id` varchar(64) NOT NULL,
 	`belonged_customer_id` varchar(64) NOT NULL,
-	`brand` text,
-	`model` text,
-	`name` text,
-	`serial` text,
+	`brand` varchar(64),
+	`model` varchar(64),
+	`name` varchar(64),
+	`serial` varchar(64),
 	`update_at` timestamp(6) NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP(6),
 	`create_at` timestamp(6) NOT NULL DEFAULT (now()),
 	CONSTRAINT `items_id` PRIMARY KEY(`id`)
@@ -70,10 +74,11 @@ CREATE TABLE `procedures` (
 
 CREATE TABLE `sessions` (
 	`id` varchar(64) NOT NULL,
-	`employee_id` varchar(64) NOT NULL,
+	`user_id` varchar(64) NOT NULL,
 	`update_at` timestamp(6) NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP(6),
 	`create_at` timestamp(6) NOT NULL DEFAULT (now()),
-	CONSTRAINT `sessions_id` PRIMARY KEY(`id`)
+	CONSTRAINT `sessions_id` PRIMARY KEY(`id`),
+	CONSTRAINT `sessions_userId_unique` UNIQUE(`user_id`)
 );
 
 CREATE TABLE `state_incompletes` (
@@ -101,15 +106,14 @@ CREATE TABLE `state_user_cancels` (
 	CONSTRAINT `state_user_cancels_state_id` PRIMARY KEY(`state_id`)
 );
 
+ALTER TABLE `employees` ADD CONSTRAINT `employees_id_users_id_fk` FOREIGN KEY (`id`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `items` ADD CONSTRAINT `items_belonged_customer_id_customers_id_fk` FOREIGN KEY (`belonged_customer_id`) REFERENCES `customers`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `order_item_map` ADD CONSTRAINT `order_item_map_order_id_orders_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `order_item_map` ADD CONSTRAINT `order_item_map_item_id_items_id_fk` FOREIGN KEY (`item_id`) REFERENCES `items`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `procedures` ADD CONSTRAINT `procedures_order_id_orders_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`) ON DELETE cascade ON UPDATE no action;
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_employee_id_employees_id_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees`(`id`) ON DELETE cascade ON UPDATE no action;
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `state_incompletes` ADD CONSTRAINT `state_incompletes_state_id_procedures_id_fk` FOREIGN KEY (`state_id`) REFERENCES `procedures`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `state_payments` ADD CONSTRAINT `state_payments_state_id_procedures_id_fk` FOREIGN KEY (`state_id`) REFERENCES `procedures`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `state_processings` ADD CONSTRAINT `state_processings_state_id_procedures_id_fk` FOREIGN KEY (`state_id`) REFERENCES `procedures`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `state_processings` ADD CONSTRAINT `state_processings_employee_id_employees_id_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees`(`id`) ON DELETE cascade ON UPDATE no action;
 ALTER TABLE `state_user_cancels` ADD CONSTRAINT `state_user_cancels_state_id_procedures_id_fk` FOREIGN KEY (`state_id`) REFERENCES `procedures`(`id`) ON DELETE cascade ON UPDATE no action;
-
-COMMIT;
